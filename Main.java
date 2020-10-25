acf3820e29be5bb7c0d531cba52e8bf6e7401120
brandonnajeeb <bn0681575@swccd.edu>
2020-10-25 16:06:25 +0000

    Added additional methods for surface area and converting inches to centimeters

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-    Scanner fromKey = new Scanner (System.in);
+    // scan functions that recieve radius in inches
 
+    Scanner fromKey = new Scanner (System.in);
     System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusCm = fromKey.nextInt();
-    /*
+    int radiusInch1 = fromKey.nextInt();
     System.out.print("Please enter an integer radius measurement (in inches):" );
     int radiusInch2 = fromKey.nextInt();
     System.out.print("Please enter an integer radius measurement (in inches):" );
@@ -15,7 +15,13 @@ class Main {
     int radiusInch4 = fromKey.nextInt();
     System.out.print("Please enter an integer radius measurement (in inches):" );
     int radiusInch5 = fromKey.nextInt();
-    */
+
+    // call to method to convert user generated radii
+
+    double radiusCm1 = Sphere.convertRadius(radiusInch1);
+
+
+    
 
     
 
@@ -24,10 +30,16 @@ class Main {
     double numValues = 5.0;
     
     System.out.println();
-    System.out.print("Radius (in.) Radius(cm.) Volume(cu. cm.) Surface Area (cm. sq.)\n");
+    System.out.print("Radius (in.) Radius(cm.) Volume(cu. cm.) Surface Area (cm. sq.)");
     System.out.print("-------------------------------------------------------------------");
     System.out.println();
 
-    System.out.println(Sphere.volume(radiusCm));
+    System.out.printf("%f, %f\n", Sphere.volume(radiusInch1), Sphere.surfaceArea(radiusInch1));
+    System.out.println(Sphere.volume(radiusInch2));
+    System.out.println(Sphere.volume(radiusInch3));
+    System.out.println(Sphere.volume(radiusInch4));
+    System.out.println(Sphere.volume(radiusInch5));
+    System.out.println(Sphere.volume(radiusCm1));
+    
   }
 }
--- Sphere.java
+++ Sphere.java
@@ -1,20 +1,21 @@
   class Sphere {
-    
+     
 
 
 
-  public static double volume (double radiusCm)
+  public static double volume (double radius)
   {
     // this will calculate volume using a given radius
-    double volume = 4.0/3.0 * Math.PI * Math.pow (radiusCm, 3.0);
+    double volume = 4.0/3.0 * Math.PI * Math.pow (radius, 3.0);
     // volume will be returned
     return volume;
   } // end of volume} // end of class 
-  /*
+  
+  
 
 // this method will calculate surface area
 
-  public static double surfaceArea (double radiusCm)
+  public static double surfaceArea (double radius)
   {
     // this will calculate the surface area using a given // radius
     double surfaceArea = 4.0 * Math.PI * Math.pow(radius, 2.0);
@@ -22,7 +23,17 @@
     return surfaceArea;
   } // end of surface area
 
-  */
+  // this method will convert inches to centimeters
+
+  public static double convertRadius (double radius)
+
+  {
+    double conversionConstant = 2.54;
+    double radiusCM = radius * conversionConstant;
+    return radiusCM;
+  }
+
+  
   
   }
   
