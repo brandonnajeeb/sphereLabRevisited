3a9b68c87c4e6f060791a8d07f1a1803979c566d
brandonnajeeb <bn0681575@swccd.edu>
2020-10-25 18:49:05 +0000

    method for print statemeent

--- Main.java
+++ Main.java
@@ -2,21 +2,14 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-    // scan functions that recieve radius in inches
+    // call to scan method in sphere to get user input
 
-    /*Scanner fromKey = new Scanner (System.in);
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInch1 = fromKey.nextInt(); */
     int radiusInch1 = Sphere.askUser(); 
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInch2 = fromKey.nextInt();
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInch3 = fromKey.nextInt();
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInch4 = fromKey.nextInt();
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInch5 = fromKey.nextInt();
-
+    int radiusInch2 = Sphere.askUser(); 
+    int radiusInch3 = Sphere.askUser(); 
+    int radiusInch4 = Sphere.askUser(); 
+    int radiusInch5 = Sphere.askUser(); 
+    
     // call to method to convert user generated radii
 
     double radiusCm1 = Sphere.convertRadius(radiusInch1);
@@ -34,11 +27,11 @@ class Main {
 
     // print functions for each row of table
 
-    System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch1, radiusCm1, Sphere.volume(radiusCm1), Sphere.surfaceArea(radiusCm1));
+    System.out.printf("%d %14.2f %13.2f %15.2f\n", /*radiusInch1, radiusCm1, Sphere.volume(radiusCm1), Sphere.surfaceArea(radiusCm1));
     System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch2, radiusCm2, Sphere.volume(radiusCm2), Sphere.surfaceArea(radiusCm2));
     System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch3, radiusCm3, Sphere.volume(radiusCm3), Sphere.surfaceArea(radiusCm3));
     System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch4, radiusCm4, Sphere.volume(radiusCm4), Sphere.surfaceArea(radiusCm4));
-    System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch5, radiusCm5, Sphere.volume(radiusCm5), Sphere.surfaceArea(radiusCm5));
+    System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusInch5, radiusCm5, Sphere.volume(radiusCm5), Sphere.surfaceArea(radiusCm5));*/
     
     
   }
--- Sphere.java
+++ Sphere.java
@@ -12,40 +12,53 @@
     return volume;
   } // end of volume} // end of class 
   
+
   
 
 // this method will calculate surface area
 
-  public static double surfaceArea (double radius2)
+  public static double surfaceArea (double radius)
   {
     // this will calculate the surface area using a given // radius
-    double surfaceArea = 4.0 * Math.PI * Math.pow(radius2, 2.0);
+    double surfaceArea = 4.0 * Math.PI * Math.pow(radius, 2.0);
     // suface area will be returned
     return surfaceArea;
   } // end of surface area
 
+
+
   // this method will convert inches to centimeters
 
-
-
-  public static double convertRadius (double radius3)
+  public static double convertRadius (double radius)
 
   {
-    double conversionConstant = 2.54;
-    double radiusCM = radius3 * conversionConstant;
+    double conversionConstant = 2.54; // inches per cm
+    double radiusCM = radius * conversionConstant;
     return radiusCM;
   } // end of conversion method
 
+
+
   // method that takes user input in inches
 
-  public static int askUser (int inchInput)
+  public static int askUser ()
   {
     Scanner fromKey = new Scanner (System.in);
-    System.out.print("Please enter an integer radius measurement (in inches):" );
-    int radiusInput = fromKey.nextInt();
+    System.out.print("Please enter an integer radius measurement (in inches):" ); //print statement to user
+    int radiusInput = fromKey.nextInt(); // capture input
     return radiusInput;
-  }
+  } // end of scanner method
 
+
+
+  // method that prints out table by row
+/*
+  public static void printRow (int radiusIn, double radiusCm1)
+  {
+    
+    System.out.printf("%d %14.2f %13.2f %15.2f\n", radiusIn, radiusCm, Sphere.volume(radiusCm1), Sphere.surfaceArea(radiusCm1));
+  }
+*/
   
   
   }// end of shere
